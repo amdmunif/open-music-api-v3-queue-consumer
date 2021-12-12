5bd21780d92c30c9ded280925f00052bd04b049f
amdmunif <ahmad.munif02@gmail.com>
2021-12-12 17:16:40 +0700

    update playslists

--- src/consumer.js
+++ src/consumer.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const amqp = require('amqplib');
-const SongsService = require('./SongsService');
+const PlaylistsService = require('./PlaylistsService');
 const MailSender = require('./MailSender');
 const Listener = require('./listener');
 
 const init = async() => {
-    const songsService = new SongsService();
+    const playlistsService = new PlaylistsService();
     const mailSender = new MailSender();
-    const listener = new Listener(notesService, mailSender);
+    const listener = new Listener(playlistsService, mailSender);
 
     const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
     const channel = await connection.createChannel();
